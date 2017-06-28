9480476b5fb1716d8a8a172f5d054549b347ba5f
sharonchen9593 <sharonchen9593@gmail.com>
2017-06-27 17:39:35 -0700

    bcrypt submitted password from login page to see if it matches the bcrypt password we have the the DB

--- router.js
+++ router.js
@@ -1,5 +1,12 @@
 var Authentication = require('./controllers/authentication');
+var passportService = require('./services/passport');
+var passport = require('passport');
+
+var requireAuth = passport.authenticate('jwt', {session: false});
 
 module.exports=function(app) {
+  app.get('/', requireAuth, function(req, res) {
+    res.send({hi:'there'});
+  });
   app.post('/signup', Authentication.signup);
 };
--- services/passport.js
+++ services/passport.js
@@ -3,9 +3,26 @@ var User = require('../models/user');
 var config = require('../config');
 var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
+var LocalStrategy = require('passport-local');
+
+//create local strategy. check if need usernameField
+var localLogin = new LocalStrategy({usernameField: 'username'}, function(email, password, done) {
+	// verify this username and password.
+	User.findOne({username: username}, function(err, user) {
+		if (err) { return done(err); }
+		//call done with false if incorrect
+		if (!user) { return done(null, false)}
+	})
+	// call done with username if correct username and pw
+	//compare password === user.password
+	
+});
 
 //Set up options for JWT strategy
-var jwtOptions = {};
+var jwtOptions = {
+	jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+	secretOrKey: config.secret
+};
 
 // create JWT strategy. Payload is user id and timestamp. done is callback function if success
 var jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
@@ -24,3 +41,4 @@ var jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
 });
 
 //Tell passport to use strategy
+passport.use(jwtLogin);
