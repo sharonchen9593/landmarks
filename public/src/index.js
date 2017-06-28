19520fbf65652e0950b33ee8adb9686154e922df
Anthony Nguyen <anguyee@gmu.edu>
2017-06-28 14:12:46 -0700

    added a register route

--- public/src/index.js
+++ public/src/index.js
@@ -3,16 +3,16 @@ import {BrowserRouter, Route} from 'react-router-dom';
 
 import App from './components/app';
 import SignIn from './components/signin';
+import Register from './components/register';
 
 ReactDOM.render(
 	<BrowserRouter>
 		<div>
-			<Route path="/" component={App}>
-			</Route>
-			<Route path="/signin" component={SignIn}>
-			</Route>
+			<Route path="/" component={App} />
+			<Route path="/signin" component={SignIn}/>
+			<Route path ="/register" component={register} />
 		</div>
 	</BrowserRouter>
-	
+
 	, document.querySelector('.container'));
 
