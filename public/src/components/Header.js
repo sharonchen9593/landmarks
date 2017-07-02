883f98a9c25bd9e1a326e431a093df269515215d
Anthony Nguyen <anguyee@gmu.edu>
2017-07-01 19:06:20 -0700

    created a dropbox for images

---
+++ public/src/components/Header.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+export const Header = () => (
+  <header>
+    <nav>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/roster'>Roster</Link></li>
+        <li><Link to='/schedule'>Schedule</Link></li>
+      </ul>
+    </nav>
+  </header>
+)
--- public/src/components/mainpage.js
+++ public/src/components/mainpage.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Dropzone from 'react-dropzone';
+import Basic from './basic'
 
 export default class MainPage extends React.Component {
 	render() {
 		return (
-			<div>
-			This is the main page
-			</div>
+      <Basic />
+
 		);
 	}
 }
