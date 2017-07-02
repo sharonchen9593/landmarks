dd39081f5a1f23f51f1b7caa08920dfae85f2901
Anthony Nguyen <anguyee@gmu.edu>
2017-07-02 14:45:18 -0700

    readded dropzone

--- public/src/components/mainpage.js
+++ public/src/components/mainpage.js
@@ -6,10 +6,7 @@ export default class MainPage extends React.Component {
 	render() {
 		return (
 
-  <form method='post' action='/upload' encType='multipart/form-data'>
-  <input type='file' name='image'/>
-  <input type='submit' /></form>
-
+<Basic />
 		);
 	}
 }
